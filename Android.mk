de4ab1199ac4a86cc434f1ee64160d61419adca0
Daveee10 <daver16@msn.com>
2012-10-20 14:43:50 +0200

    Some fix for "Move PanoramaActivity to root folder"

--- Android.mk
+++ Android.mk
@@ -9,11 +9,13 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := LegacyCamera
 #LOCAL_SDK_VERSION := current
+endif
 
 LOCAL_JNI_SHARED_LIBRARIES := libjni_legacymosaic
 
 LOCAL_REQUIRED_MODULES := libjni_legacymosaic
 
+ifneq (,$(findstring Gallery2,$(PRODUCT_PACKAGES)))
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
